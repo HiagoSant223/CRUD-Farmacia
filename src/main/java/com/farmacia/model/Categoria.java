ae9c9b6021b82e61c327dda339abad166335c0af
Hiago Oliveira <hiagosantos223@gmail.com>
2024-04-12 12:10:16 -0300

    Construção Model, Repository e Controller Categoria

---
+++ src/main/java/com/farmacia/model/Categoria.java
@@ -0,0 +1,51 @@
+package com.farmacia.model;
+
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O nome é obrigatorio")
+	private String nome;
+	
+	private String descricao;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+}
---
+++ src/main/java/com/farmacia/repository/CategoriaRepository.java
@@ -0,0 +1,13 @@
+package com.farmacia.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.farmacia.model.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
+
+	public List<Categoria> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);
+}
